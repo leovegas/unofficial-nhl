5a7861e080fe8da8df5435e32835a4e95db7d9cd
leonid <timofeevld@mail.com>
2021-01-16 23:28:02 +0100

    navbar and actionbar implemented

---
+++ app/src/main/java/com/app/unofficial_nhl/MainActivity2.java
@@ -0,0 +1,164 @@
+package com.app.unofficial_nhl;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+import com.app.unofficial_nhl.pojos.Record;
+import com.app.unofficial_nhl.pojos.TeamRecord;
+import com.app.unofficial_nhl.pojos.Teams;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.*;
+
+import static androidx.appcompat.app.ActionBar.DISPLAY_USE_LOGO;
+import static androidx.appcompat.app.AppCompatDelegate.FEATURE_SUPPORT_ACTION_BAR;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        //actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        ViewGroup v = (ViewGroup) LayoutInflater.from(this)
+                .inflate(R.layout.custom_actionbar, null);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM,
+                ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(v,
+                new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT,
+                        ActionBar.LayoutParams.WRAP_CONTENT,
+                        Gravity.CENTER_VERTICAL | Gravity.RIGHT));
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar != null)
+        {
+            setupActionBar();
+
+        }
+
+        // Get a support ActionBar corresponding to this toolbar
+
+        // Enable the Up button
+
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getPlayerInfoById(8477474)
+                .enqueue(new Callback<Teams>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Teams> call, @NonNull Response<Teams> response) {
+                        Teams people = response.body();
+
+                        System.out.println(people.getPeople().get(0).getFullName());
+                        System.out.println(people.getPeople().get(0).getBirthCity());
+                        System.out.println(people.getPeople().get(0).getBirthCountry());
+                        System.out.println(people.getPeople().get(0).getLink());
+
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Teams> call, @NonNull Throwable t) {
+                        System.out.println("Error occurred while getting request!");
+                        t.printStackTrace();
+                    }
+
+                });
+
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getStandings()
+                .enqueue(new Callback<Teams>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Teams> call, @NonNull Response<Teams> response) {
+                        Map<Integer, String> standings = new TreeMap<>();
+                        Teams teams = response.body();
+                        for (Record team : teams.getRecords()) {
+                            for (TeamRecord t : team.getTeamRecords()) {
+                                standings.put(Integer.valueOf(t.getLeagueRank()), t.getTeam().getName());
+                            }
+                        }
+                        System.out.println(standings);
+
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Teams> call, @NonNull Throwable t) {
+                        System.out.println("Error occurred while getting request!");
+                        t.printStackTrace();
+                    }
+
+                });
+
+//        final ListView listview = (ListView) findViewById(R.id.listview);
+//        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+//                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+//                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+//                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+//                "Android", "iPhone", "WindowsMobile" };
+//
+//        final ArrayList<String> list = new ArrayList<String>(Arrays.asList(values));
+//        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.closest_games_list,
+//                R.id.textView5, list);
+//        listview.setAdapter(adapter);
+//
+//    }
+//
+//    private static class StableArrayAdapter extends ArrayAdapter<String> {
+//
+//        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+//
+//        public StableArrayAdapter(Context context, int textViewResourceId,
+//                                  List<String> objects) {
+//            super(context, textViewResourceId, objects);
+//            for (int i = 0; i < objects.size(); ++i) {
+//                mIdMap.put(objects.get(i), i);
+//            }
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            String item = getItem(position);
+//            return mIdMap.get(item);
+//        }
+//
+//        @Override
+//        public boolean hasStableIds() {
+//            return true;
+//        }
+//
+//    }
+    }
+
+}
---
+++ app/src/main/java/com/app/unofficial_nhl/ui/home/HomeFragment.java
@@ -0,0 +1,77 @@
+package com.app.unofficial_nhl.ui.home;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import com.app.unofficial_nhl.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =  ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        final ListView listview = (ListView) root.findViewById(R.id.listview);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+
+        final ArrayList<String> list = new ArrayList<String>(Arrays.asList(values));
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(root.getContext(), R.layout.closest_games_list,
+                R.id.textView5, list);
+        listview.setAdapter(adapter);
+        return root;
+
+
+            }
+    private static class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+}
